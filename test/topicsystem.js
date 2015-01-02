d69fc99353f728afec7dec949556d6eaf74aeaf6
Rob Ellis <rob@chloi.io>
2015-01-01 20:36:48 -0800

    Mix in other topics that don't have search terms

--- test/topicsystem.js
+++ test/topicsystem.js
@@ -3,12 +3,12 @@ var should  = require("should");
 var help = require("./helpers");
 
 // Testing topics that include and mixin other topics.
-describe('SuperScript TopicsSystem', function(){
+describe.only('SuperScript TopicsSystem', function(){
 
   before(help.before("topicsystem"));
 
   describe('TopicSystem', function() {
-    it("Should skip empty replies until it finds a match", function(done){
+    it.only("Should skip empty replies until it finds a match", function(done){
       bot.reply("testing topic system", function(err, reply){
         ["we like it","i hate it"].should.containEql(reply);
         done();
@@ -16,17 +16,15 @@ describe('SuperScript TopicsSystem', function(){
     });
   });
 
-  describe.only('TopicDiscovery', function() {
+  describe('TopicDiscovery', function() {
     it("Should find the right topic", function(done){
       bot.reply("i like to hunt", function(err, reply){
         reply.should.containEql("i like to spend time outdoors");
 
-        done();
-
-        // bot.reply("i like to fish", function(err, reply){
-        //   reply.should.containEql("me too");
-        //   done();
-        // });
+        bot.reply("i like to fish", function(err, reply){
+          reply.should.containEql("me too");
+          done();
+        });
 
       });
     });
