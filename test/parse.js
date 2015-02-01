97e69337235da4d38861fdeeb4f614723bb45fdb
Rob Ellis <rob@chloi.io>
2015-02-01 01:58:26 -0800

    New Topic System with Mongo

--- test/parse.js
+++ test/parse.js
@@ -1,22 +1,17 @@
 var mocha = require("mocha");
 var should  = require("should");
+var mongoose = require("mongoose");
 
-
-var parse = require("../lib/parse")();
+var importFile = require("../lib/topics/import")();
+mongoose.connect("mongodb://localhost/topicDB");
 
 // This is to add some extra functionity to the parse engine.
 describe.only('parse interface', function(){
 
-  it("should parse single string", function(done){
-
-    // Lets take a random string and parse it.
-    var str = "? This be *~2 random <noun1> to say\n - Say it isn't so";
-    var topic = "random";
-
-    parse.parseContents(str, null, function(err, res){
-      console.log(JSON.stringify(res, null, 2));
-      done();
+  it("should import file to topic Data", function(done){
+    importFile('./test/fixtures/cache/script.json', function(err, res){
+      res.should.eql("done");
+      done()
     });
-
   });
 });
