3a23ec42d2a37eb2c38adc78134a7bb9f8de47e5
Rob Ellis <rob@chloi.io>
2015-03-10 19:58:03 -0700

    Adds mongoose to bot-init script

--- bin/bot-init.js
+++ bin/bot-init.js
@@ -58,6 +58,7 @@ fs.mkdir(botPath, function(err, res){
     , dependencies: {
        'superscript': 'latest'
       , 'sfacts':'latest'
+      , 'mongoose':'3.8.24'
       ,'debug': '~2.0.0'
     }
   }
