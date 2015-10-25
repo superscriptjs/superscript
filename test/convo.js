2e71f7e6a0069f8de6f838797be04f660c2b54e4
Marijan Karovski <m.karovski@gmail.com>
2015-10-25 17:31:12 +0100

    Adds additional conversation tests for interaction with last reply

--- test/convo.js
+++ test/convo.js
@@ -6,13 +6,53 @@ describe('Super Script Conversation', function(){
 
   before(help.before("convo"));
 
+  describe('Conversations', function(){
+
+    it("should not trigger conversation - no last reply", function(done) {
+      bot.reply("user1", "redirect testconversation trigger two", function(err, reply) {
+        reply.string.should.eql("trigger two test ok. lastreply does not exist.");
+        done();
+      });
+    });
+
+    it("should not trigger wildcard conversation - no last reply", function(done) {
+      bot.reply("user1", "test testconversation wildcard in lastreply", function(err, reply) {
+        reply.string.should.eql("matched by catch all wildcard.");
+        done();
+      });
+    });
+
+    it("should trigger first reply", function(done) {
+      bot.reply("user1", "redirect testconversation trigger one", function(err, reply) {
+        reply.string.should.eql("trigger one test ok");
+        done();
+      });
+    });
+
+    it("should trigger conversation from last reply", function(done) {
+      bot.reply("user1", "redirect testconversation trigger two", function(err, reply) {
+        reply.string.should.eql("trigger two test ok. lastreply is trigger one test ok.");
+        done();
+      });
+    });
+
+    it("should trigger wildcard from last reply", function(done) {
+      bot.reply("user1", "redirect testconversation wildcard in lastreply", function(err, reply) {
+        reply.string.should.eql("wildcard in lastreply test ok");
+        done();
+      });
+    });
+
+  });
+
+
   describe('Volley', function(){
-    
+
     it("should have volley", function(done) {
       bot.reply("user1", "Can you skip rope?", function(err, reply) {
         bot.getUser("user1", function(e, user){
           user.volley.should.eql(0);
-          done();          
+          done();
         });
       });
     });
@@ -30,10 +70,11 @@ describe('Super Script Conversation', function(){
               done();
             });
           });
-        });        
+        });
       });
     });
+
   });
-  
+
   after(help.after);
 });
--- test/fixtures/convo/main.ss
+++ test/fixtures/convo/main.ss
@@ -1,8 +1,40 @@
-+ Can you skip rope?
-- yep
+> topic basic
 
-+ Can you jump rope?
-  - yep, can you?
+  + Can you skip rope?
+  - yep
+
+  + Can you jump rope?
+    - yep, can you?
+    + Have you done it lately?
+    % ~yes, can you?
+    - We should hang out sometime.
+
+< topic
+
+> topic __pre__
+
+  + redirect *1 (*)
+  - ^topicRedirect(<cap1>,<cap2>)
+
+< topic
+
+> topic:system:keep testconversation
+
+  + trigger one
+  - trigger one test ok
+
+  + trigger two
+  % trigger one test ok
+  - trigger two test ok. lastreply is trigger one test ok.
+
+  + wildcard in lastreply
+  % * lastreply is trigger one *
+  - wildcard in lastreply test ok
+
+  + trigger two
+  - trigger two test ok. lastreply does not exist.
+
   + *
-  % ~yes, can you?
-  - We should hang out sometime.
+  - matched by catch all wildcard.
+
+< topic
