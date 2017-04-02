5fe7ffd57bbb55a6bf6f839def0151abb1b28245
Hain Wang <hailiang.hl.wang@gmail.com>
2017-04-03 07:17:26 +0800

    Checkout chat history data with a property in options (#333)
    * Checkout chat history data with a property in options
    
    * Pass historyCheckpoints in SuperScript's constructor

--- src/bot/index.js
+++ src/bot/index.js
@@ -14,7 +14,7 @@ import Logger from './logger';
 const debug = debuglog('SS:SuperScript');
 
 class SuperScript {
-  constructor(coreChatSystem, coreFactSystem, plugins, scope, editMode, conversationTimeout, tenantId = 'master') {
+  constructor(coreChatSystem, coreFactSystem, plugins, scope, editMode, conversationTimeout, historyCheckpoints, tenantId = 'master') {
     this.chatSystem = coreChatSystem.getChatSystem(tenantId);
     this.factSystem = coreFactSystem.getFactSystem(tenantId);
 
@@ -30,6 +30,7 @@ class SuperScript {
     this.plugins = plugins;
     this.editMode = editMode;
     this.conversationTimeout = conversationTimeout;
+    this.historyCheckpoints = historyCheckpoints;
   }
 
   importFile(filePath, callback) {
@@ -46,7 +47,7 @@ class SuperScript {
 
   getUser(userId, callback) {
     this.chatSystem.User.findOne({ id: userId })
-      .slice('history', 10)
+      .slice('history', this.historyCheckpoints)
       .exec(callback);
   }
 
@@ -55,7 +56,7 @@ class SuperScript {
       upsert: true,
       setDefaultsOnInsert: true,
       new: true,
-    }).slice('history', 10)
+    }).slice('history', this.historyCheckpoints)
       .exec(callback);
   }
 
@@ -181,6 +182,7 @@ class SuperScriptInstance {
     // This is a kill switch for filterBySeen which is useless in the editor.
     this.editMode = options.editMode || false;
     this.conversationTimeout = options.conversationTimeout;
+    this.historyCheckpoints = options.historyCheckpoints;
     this.scope = options.scope || {};
 
     // Built-in plugins
@@ -223,6 +225,7 @@ class SuperScriptInstance {
       this.scope,
       this.editMode,
       this.conversationTimeout,
+      this.historyCheckpoints,
       tenantId,
     );
   }
@@ -242,6 +245,7 @@ const defaultOptions = {
   logPath: `${process.cwd()}/logs`,
   useMultitenancy: false,
   conversationTimeout: 1000 * 300,
+  historyCheckpoints: 10
 };
 
 /**
